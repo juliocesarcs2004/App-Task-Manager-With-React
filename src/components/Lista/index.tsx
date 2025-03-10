52cfbd5970485ea264f519e59bb28efd0a346019
juliocesarcs2004 <juliocesarcs2004@gmail.com>
2025-03-10 12:53:03 -0300

    Added hooks in lists tasks and pages

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,18 +1,9 @@
-import React from 'react';
 import style from './Lista.module.scss';
 import Item from './Item';
+import { ITarefa } from '../../types/tarefa';
 
-function Lista() {
-    const tarefas = [{
-        tarefa: 'React',
-        tempo: '02:00:00'
-    }, {
-        tarefa: 'JavaScript',
-        tempo: '01:00:00'
-    }, {
-        tarefa: 'Typescript',
-        tempo: '03:00:00'
-    }]
+
+function Lista({ tarefas }: { tarefas: ITarefa[] }) {
     return (
         <aside className={style.listaTarefas}>
             <h2> Estudos do dia </h2>
