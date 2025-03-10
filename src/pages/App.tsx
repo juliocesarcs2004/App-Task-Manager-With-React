7f435f1d7d8a51be6abea91adc3c8d5983e7ca46
juliocesarcs2004 <juliocesarcs2004@gmail.com>
2025-03-10 17:41:57 -0300

    Rendered selected Item

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -5,13 +5,24 @@ import style from './App.module.scss';
 import Cronometro from '../components/Cronometro';
 import { ITarefa } from '../types/tarefa';
 
-
 function App() {
   const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+  const [selecionado, setSelecionado] = useState<ITarefa>();
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa) {
+    setSelecionado(tarefaSelecionada);
+    setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => ({
+      ...tarefa,
+      selecionado: tarefa.id == tarefaSelecionada.id ? true : false
+    })))
+  }
   return (
     <div className={style.AppStyle}>
       <Formulario setTarefas={setTarefas} />
-      <Lista tarefas={tarefas} />
+      <Lista
+        tarefas={tarefas}
+        selecionaTarefa={selecionaTarefa}
+      />
       <Cronometro />
     </div>
   );
