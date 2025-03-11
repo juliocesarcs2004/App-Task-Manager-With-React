cce28588690588031b352b7cd371bafad221bac8
juliocesarcs2004 <juliocesarcs2004@gmail.com>
2025-03-11 11:53:07 -0300

    Implemented Dynamic Timer

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,5 +1,5 @@
 import { ITarefa } from '../../../types/tarefa';
-import style from '../Lista.module.scss';
+import style from './Item.module.scss';
 
 interface Props extends ITarefa {
     selecionaTarefa: (tarefaSelecionada: ITarefa) => void
@@ -12,12 +12,13 @@ export default function Item(
         selecionado,
         completado,
         id,
-        selecionaTarefa }: Props) {
-    console.log('item atual: ', { tarefa, tempo, selecionado, completado, id });
+        selecionaTarefa
+    }: Props) {
     return (
         <li
-            className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`}
-            onClick={() => selecionaTarefa(
+            className={`${style.item} ${selecionado ? style.itemSelecionado : ''} 
+            ${completado ? style.itemCompletado : ''}`}
+            onClick={() => !completado && selecionaTarefa(
                 {
                     tarefa,
                     tempo,
@@ -29,6 +30,8 @@ export default function Item(
         >
             <h3> {tarefa} </h3>
             <span> {tempo} </span>
+            {completado && <span className={style.concluido}
+                aria-label="tarefa completada "></span>}
         </li>
     )
 }
